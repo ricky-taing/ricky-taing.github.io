aad9dde6430c34ab774c4abd7a3e722c86c9d0f0
Ricky102 <ricky.r.taing@gmail.com>
2024-05-14 19:03:24 -0400

    added anime data table, fetching pokemon API, small blog name changes

--- myCtrl.js
+++ myCtrl.js
@@ -11,11 +11,25 @@ app.controller("testCtrl", function($scope, $http, $interval) {
         {name: "Slime Second Season"},
         {name: "Oshi no Ko"}
     ];
+    $scope.animeData = [
+        {
+            "name": "Yamada-kun to Lv999 no Koi wo Suru",
+            "review": "Plan to Watch"
+        },
+        {
+            "name": "Natsume Yuujinchou San",
+            "review": "My favorite season of Natsume series. Favorite episodes are EP 3 and 4."
+        }
+    ]
 
     // Testing http service
     // $http.get("./test.html").then(function (response) {
-    $http.get("https://pokeapi.co/api/v2/pokemon/charmander").then(function (response) {
+    $http.get("https://pokeapi.co/api/v2/pokemon/charmander")
+    .then(function (response) {
         $scope.serverRes = response.data.name;
+    }, function (response) {
+        $scope.err = "Error fetching Pokemon data"
+        $scope.status = response.statusText;
     })
 
     // Dynamic Time
