24cc9f0b6b5c78fcbf48e4307bf90eebe0e3e295
Ricky102 <ricky.r.taing@gmail.com>
2024-05-16 18:49:22 -0400

    create and display a list of anime

---
+++ personal-website-angular/src/app/anime-list.ts
@@ -0,0 +1,14 @@
+import { Anime } from './anime';
+
+export const ANIME_LIST: Anime[] = [
+  { id: 1, name: 'Natsume Yuujinchou Movie' },
+  { id: 2, name: 'Slime Season 2' },
+  { id: 3, name: 'Oshi no Ko' },
+  { id: 4, name: 'Kaiju No. 8' },
+  { id: 5, name: 'Konosuba Season 3' },
+  { id: 6, name: 'Demon Slayer' },
+  { id: 7, name: 'Mushoku Tensei' },
+  { id: 8, name: 'Spice and Wolf' },
+  { id: 9, name: 'Yuru Camp' },
+  { id: 10, name: 'Haikyuu Dumpster Battle' }
+];
--- personal-website-angular/src/app/anime/anime.component.ts
+++ personal-website-angular/src/app/anime/anime.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { Anime } from './anime';
 import { UpperCasePipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Anime } from '../anime';
+import { ANIME_LIST } from '../anime-list';
 
 @Component({
   selector: 'app-anime',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor],
   templateUrl: './anime.component.html',
   styleUrl: './anime.component.css'
 })
+
 export class AnimeComponent {
-  anime: Anime = {
-    id: 1,
-    name: 'Natsume Yuujinchou Shi'
-  };
+  anime: 
+    Anime = {
+      id: 1,
+      name: 'Natsume Yuujinchou Shi'
+    };
+  animeList = ANIME_LIST;
 }
