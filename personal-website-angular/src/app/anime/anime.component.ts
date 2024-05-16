8d9a0854bf66d6f990d96382b0356461f169568d
Ricky102 <ricky.r.taing@gmail.com>
2024-05-16 19:35:00 -0400

    clicking on button changes its color and displays details underneath

--- personal-website-angular/src/app/anime/anime.component.ts
+++ personal-website-angular/src/app/anime/anime.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
-import { NgFor } from '@angular/common';
+import { 
+  NgFor,
+  NgIf,
+  UpperCasePipe,
+} from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { Anime } from '../anime';
 import { ANIME_LIST } from '../anime-list';
 
 @Component({
   selector: 'app-anime',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './anime.component.html',
   styleUrl: './anime.component.css'
 })
 
 export class AnimeComponent {
-  anime: 
-    Anime = {
-      id: 1,
-      name: 'Natsume Yuujinchou Shi'
-    };
   animeList = ANIME_LIST;
+  selectedAnime?: Anime;
+
+  onSelect(anime: Anime): void {
+    this.selectedAnime = anime;
+  }
 }
