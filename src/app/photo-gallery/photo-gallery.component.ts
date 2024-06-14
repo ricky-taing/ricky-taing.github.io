6020b17b6df7c48a1dfa63d1a179257ca5b862b4
Ricky102 <ricky.r.taing@gmail.com>
2024-06-14 18:24:06 -0400

    add gallery to template, set up routing

--- src/app/photo-gallery/photo-gallery.component.ts
+++ src/app/photo-gallery/photo-gallery.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { HammerModule } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
 
 @Component({
   selector: 'app-photo-gallery',
   standalone: true,
-  imports: [Angular2ImageGalleryModule, HammerModule],
+  imports: [Angular2ImageGalleryModule, HammerModule, RouterLink],
   templateUrl: './photo-gallery.component.html',
   styleUrl: './photo-gallery.component.css'
 })
 export class PhotoGalleryComponent {
 
+  yourNotificationFunction(): void {
+
+  }
 }
