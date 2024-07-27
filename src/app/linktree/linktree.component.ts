619015166db9e422d7df6dd564bd5c0e8fab927f
Ricky102 <ricky.r.taing@gmail.com>
2024-07-26 22:37:27 -0400

    use icons as links

--- src/app/linktree/linktree.component.ts
+++ src/app/linktree/linktree.component.ts
@@ -13,7 +13,6 @@ const LINKTREE_DATA = [
   {name: 'Github', description: 'Check out my other projects', link: 'https://github.com/ricky-taing'},
   {name: 'LinkedIn', description: 'Send me a message', link: 'https://www.linkedin.com/in/ricky-taing-30b358185/'},
   {name: 'Email', description: 'Contact me at ricky.r.taing@gmail.com', link: 'mailto:ricky.r.taing@gmail.com'},
-  // {name: 'MyAnimeList', description: 'View my entire list of anime', link: 'https://myanimelist.net/profile/55Ricky'},
 ];
 
 @Component({
